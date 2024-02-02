ee0f0e385fed46fb598c90a0970a015b70769808
masonmenges <mason.menges2@gmail.com>
2024-02-02 07:26:51 -0700

    initial commit

---
+++ flows/suspend_resume.py
@@ -0,0 +1,32 @@
+import time
+import os
+from prefect import task, flow
+from prefect_dask import DaskTaskRunner
+
+
+from dask_kubernetes.operator import KubeCluster
+
+
+# print(os.environ.get("KUBECONFIG", "~/.kube/config"))
+
+
+# cluster = KubeCluster(name="prefect-dask-cluster",
+#  image='ghcr.io/dask/dask:latest',
+#  n_workers=2,
+#  resources={"requests": {"memory": "0.5Gi"}, "limits": {"memory": "1.5Gi"}},
+#  env={"FOO" : "barr"}
+#  )
+
+
+
+@task
+def some_task():
+    print("Hello world!")
+    time.sleep(10)
+
+@flow(task_runner=DaskTaskRunner(address="127.0.0.1:8080"))
+def dask_test_flow():
+    results = [some_task.submit() for _ in range(2)]
+
+
+dask_test_flow()
---
+++ flows/wrapped_flow_test.py
@@ -0,0 +1,67 @@
+from prefect import flow, task, get_run_logger
+import smtplib
+import os
+import random
+import sys
+from prefect.runner.storage import GitRepository
+from prefect.blocks.system import Secret
+from functools import wraps
+
+
+
+    
+def prefect_flow_on_completion(flow, flow_run, state):
+    print("This is in an on_completion hook")
+    return
+
+def prefect_flow_on_failure(flow, flow_run, state):
+    print("This is in an on_failure hook")
+    return
+
+
+def wrapped_flow(**kwargs):
+    return flow(
+        on_failure=[prefect_flow_on_failure],
+        on_completion=[prefect_flow_on_completion],
+        **kwargs
+    )
+
+def email_with_args(msg, email):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            print(msg)
+            print("Extra logic here")
+            result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
+
+
+@email_with_args(
+    msg="This is in the start wrapper",
+    email="mason@prefect.io")
+@wrapped_flow()
+def hello_flow():
+    logger = get_run_logger()
+    logger.info("Hello world!")
+    logger.info("Going to send an email now...")
+    random_number = random.randint(1, 10)
+
+    if random_number % 2 == 0:
+        raise ValueError("Random number is even, so we'll fail the flow")
+    else:
+        return 
+
+
+if __name__ == "__main__":
+    # hello_flow()
+    flow.from_source(
+        source=GitRepository(
+            url="https://github.com/masonmenges/mm2-sanbox.git"
+        ),
+        entrypoint="flows/wrapped_flow_test.py:hello_flow"
+    ).deploy(
+        name=f"decorated_and_wrapped_flow",
+        work_pool_name="local-dev",
+        build=False
+    )
