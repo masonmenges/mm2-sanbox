47b2f6d6059156556b385be5ead2c465a6b092d1
masonmenges <mason.menges2@gmail.com>
2024-03-06 12:05:07 -0700

    add long_running_task

--- flows/wrapped_flow_test.py
+++ flows/wrapped_flow_test.py
@@ -2,6 +2,7 @@ from prefect import flow, task, get_run_logger, deploy, suspend_flow_run
 import random
 import sys
 from functools import wraps
+import time
 
 def prefect_flow_on_completion(flow, flow_run, state):
     print("This is in an on_completion hook")
@@ -19,11 +20,19 @@ def wrapped_flow(**kwargs):
         **kwargs
     )
 
+@task
+def some_long_running_task():
+    logger = get_run_logger()
+    logger.info("This is a long running task")
+    time.sleep(180)
+
 @wrapped_flow()
 def hello_flow():
     logger = get_run_logger()
     logger.info("Hello world!")
     random_number = random.randint(1, 10)
+
+    task_futures = [some_long_running_task.submit() for _ in range(5)]
 
     # if random_number % 2 == 0:
     #     raise ValueError("Random number is even, so we'll fail the flow")
@@ -32,15 +41,16 @@ def hello_flow():
 
 
 if __name__ == "__main__":
-    test = hello_flow.to_deployment(
-    name=f"decorated_and_wrapped_flow"
-    )
-    test.storage=GitRepository(url="https://github.com/masonmenges/mm2-sanbox.git")
-    test.entrypoint = "flows/wrapped_flow_test.py:hello_flow"
-    deploy(
-        test,
-        work_pool_name="docker-test",
-        image=DeploymentImage(
-                    name="masonm2/temprepo:hello_flow",
-                    dockerfile="./Dockerfile",
-                ))
+    hello_flow()
+    # test = hello_flow.to_deployment(
+    # name=f"decorated_and_wrapped_flow"
+    # )
+    # test.storage=GitRepository(url="https://github.com/masonmenges/mm2-sanbox.git")
+    # test.entrypoint = "flows/wrapped_flow_test.py:hello_flow"
+    # deploy(
+    #     test,
+    #     work_pool_name="docker-test",
+    #     image=DeploymentImage(
+    #                 name="masonm2/temprepo:hello_flow",
+    #                 dockerfile="./Dockerfile",
+    #             ))
