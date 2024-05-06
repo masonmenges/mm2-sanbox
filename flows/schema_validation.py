067dae338fdaaa7e16bfceef2e535e2c557efdc3
masonmenges <mason.menges2@gmail.com>
2024-05-06 11:18:41 -0600

    schema testing2

--- flows/schema_validation.py
+++ flows/schema_validation.py
@@ -1,19 +1,39 @@
 from pandera.typing import DataFrame, Series
+from pydantic import BaseModel
 from pandera import DataFrameModel
+from pandera.engines.pandas_engine import PydanticModel
 from prefect import flow, task, get_run_logger
 
+class SampleContract(BaseModel):
+    field_1: str
+    field_2: int
 
-class SampleContract(DataFrameModel):
-    field_1: Series[str]
-    field_2: Series[int]
 
+# class SampleContract(DataFrameModel):
+#     field_1: Series[str]
+#     field_2: Series[int]
 
-class SampleContract2(DataFrameModel):
-    fruit: Series[str]
+class PydanticSampleContract(DataFrameModel):
+
+    class config:
+        dtype = PydanticModel(SampleContract)
+        coerce = True
+
+class SampleContract2(BaseModel):
+    fruit: str
+
+class PydanticSampleContract2(DataFrameModel):
+    
+    class config:
+        dtype = PydanticModel(SampleContract2)
+        coerce = True
+
+# class SampleContract2(DataFrameModel):
+#     fruit: Series[str]
 
 
 @task
-def add_fruits(input: DataFrame[SampleContract]) -> DataFrame[SampleContract2]:
+def add_fruits(input: DataFrame[PydanticSampleContract]) -> DataFrame[PydanticSampleContract2]:
     output = input.copy()
     output["fruit"] = ["apple", "banana"]
     return output
@@ -23,8 +43,8 @@ def add_fruits(input: DataFrame[SampleContract]) -> DataFrame[SampleContract2]:
     name="test_flow",
 )
 def test_flow(
-    input: DataFrame[SampleContract] = {"field_1": ["val1", "val2"], "field_2": [1, 2]},  # type: ignore[assignment]
-) -> DataFrame[SampleContract2]:
+    input: DataFrame[PydanticSampleContract] = {"field_1": ["val1", "val2"], "field_2": [1, 2]},  # type: ignore[assignment]
+) -> DataFrame[PydanticSampleContract2]:
     logger = get_run_logger()
     logger.info(f"Data at Start of Flow: {input}")
 
@@ -32,7 +52,7 @@ def test_flow(
 
     logger.info(f"Data at End of Flow: {output}")
 
-    return SampleContract2.validate(output)  # type: ignore[return-value]
+    return PydanticSampleContract2.validate(output)  # type: ignore[return-value]
 
 
 if __name__ == "__main__":
