8c980d945013232bebb97a75ed6ecb8677c6dfcc
masonmenges <mason.menges2@gmail.com>
2024-05-07 13:15:20 -0600

    pandera schema testing6

--- flows/schema_validation.py
+++ flows/schema_validation.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel 
 from pydantic.types import List
 from prefect import flow, task, get_run_logger
 
@@ -11,29 +11,29 @@ class SampleContract2(BaseModel):
     fruit: List[str]
 
 
-@task
+# @task
 def add_fruits(input: SampleContract) -> SampleContract2:
     output = input.model_dump()
     output["fruit"] = ["apple", "banana"]
     return output
 
 
-@flow(
-    name="test_flow",
-    validate_parameters=True
-)
+# @flow(
+#     name="test_flow",
+#     validate_parameters=True
+# )
 def test_flow(
-    input: SampleContract = {"field_1": ["val1", "val2"], "field_2": [1, 2]},  # type: ignore[assignment]
+    input: SampleContract = SampleContract.model_validate({"field_1": ["val1", "val2"], "field_2": [1, 2]}),  # type: ignore[assignment]
 ) -> SampleContract2:
-    logger = get_run_logger()
-    logger.info(f"Data at Start of Flow: {input}")
+    # logger = get_run_logger()
+    # logger.info(f"Data at Start of Flow: {input}")
 
     output = add_fruits(input)
 
-    logger.info(f"Data at End of Flow: {output}")
+    # logger.info(f"Data at End of Flow: {output}")
 
     return SampleContract2.model_validate(output)  # type: ignore[return-value]
 
 
 if __name__ == "__main__":
-    test_flow()
+    test_flow(input={"field_1": ["val1", "val2"], "field_2": [1, 2]})
