ca8b686b1d4b48000760bfd75ad274c83f505e5c
masonmenges <mason.menges2@gmail.com>
2024-06-18 13:05:55 -0600

    result persistence tests8

--- flows/test_result_persistence.py
+++ flows/test_result_persistence.py
@@ -8,12 +8,12 @@ def persist_test():
     passing_task()
     failing_task()
 
-@task
+@task(persist_result=True)
 def passing_task():
     print("This task should be skipped on retry")
     return 42
 
-@task
+@task(persist_result=True)
 def failing_task():
     raise ValueError("This task failed")
 
