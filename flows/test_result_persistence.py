8a6bcc6e2378174e9ef69c826aca1ccb7428380b
masonmenges <mason.menges2@gmail.com>
2024-06-18 11:15:01 -0600

    result persistence tests

--- flows/test_result_persistence.py
+++ flows/test_result_persistence.py
@@ -1,6 +1,8 @@
 from prefect import flow, task
+from prefect.runner.storage import GitRepository
+from prefect.filesystems import LocalFileSystem
 
-@flow(log_prints=True)
+@flow(log_prints=True, result_storage=LocalFileSystem(basepath="/Users/masonmenges/Desktop/flowresults/"))
 def persist_test():
     passing_task()
     failing_task()
@@ -15,4 +17,17 @@ def failing_task():
     raise ValueError("This task failed")
 
 if __name__=="__main__":
-    persist_test.serve()
+    # persist_test.serve()
+
+    persist_test.from_source(
+            source=GitRepository(
+                url="https://github.com/masonmenges/mm2-sanbox.git"
+                ),
+            entrypoint="flows/test_result_persistence.py:persist_test",
+        ).deploy(
+        name="result_persistence-test",
+        work_pool_name="default",
+        version="local_demo:0.0.1",
+        # schedules=[schedule_1],
+        enforce_parameter_schema=True,
+        )
