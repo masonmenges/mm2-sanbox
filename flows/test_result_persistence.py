2ee8c7fae3094c6c8b2de5015dd0d9820e58cda4
masonmenges <mason.menges2@gmail.com>
2024-06-18 13:54:03 -0600

    result persistence tests04

--- flows/test_result_persistence.py
+++ flows/test_result_persistence.py
@@ -4,7 +4,7 @@ from prefect.tasks import task_input_hash
 from prefect.filesystems import LocalFileSystem, S3
 from prefect_aws import S3Bucket
 
-@flow(log_prints=True, result_storage=S3(bucket_path="mm2-bucket"), persist_result=True)
+@flow(log_prints=True, result_storage=S3Bucket.load("mm2-bucket"), persist_result=True)
 def persist_test():
     passing_task()
     failing_task()
