0f3d879ac131b2a754446bb3d2cecf2b8420d5f6
masonmenges <mason.menges2@gmail.com>
2024-06-20 10:53:02 -0600

    3.x result persistence test1

--- flows/test_result_persistence.py
+++ flows/test_result_persistence.py
@@ -1,10 +1,7 @@
 from prefect import flow, task
 from prefect.runner.storage import GitRepository
-from prefect.tasks import task_input_hash
-from prefect.filesystems import LocalFileSystem, RemoteFileSystem
-from prefect_aws import S3Bucket, AwsCredentials
+from prefect.filesystems import RemoteFileSystem
 
-creds = AwsCredentials.load("mm2-se-dev")
 
 @flow(
         log_prints=True,
@@ -27,17 +24,12 @@ def failing_task():
     raise ValueError("This task failed")
 
 if __name__=="__main__":
-    # persist_test.serve()
-
     persist_test.from_source(
             source=GitRepository(
                 url="https://github.com/masonmenges/mm2-sanbox.git"
                 ),
             entrypoint="flows/test_result_persistence.py:persist_test",
         ).deploy(
-        name="result_persistence-test",
+        name="result_persistence-test_3.x",
         work_pool_name="default",
-        version="local_demo:0.0.1",
-        # schedules=[schedule_1],
-        enforce_parameter_schema=True,
         )
