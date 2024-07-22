085a2226c9d0e91a3ffde9f0849394e05d6099cf
masonmenges <mason.menges2@gmail.com>
2024-07-22 09:33:35 -0600

    update concurrency with timeout

--- flows/concurrency.py
+++ flows/concurrency.py
@@ -1,71 +1,35 @@
 from prefect import flow, task, get_run_logger
-from prefect.concurrency.asyncio import concurrency
-import time
-
-from prefect.deployments.runner import DeploymentImage 
-from prefect.runner.storage import GitRepository
-from prefect.client.schemas.schedules import CronSchedule
-
-
-CONCURRENCY_CONFIG = "concurrency-test-limit-1"
+import os
+from prefect.concurrency.asyncio import concurrency, rate_limit
+import datetime
+import asyncio
+import numpy as np
 
 
 @task
-async def long_running_task():
+async def long_running_task(n: int):
     logger = get_run_logger()
+    logger.info(f"Task {n} running..")
+    await asyncio.sleep(5)
 
-    logger.info(f"Using concurrency limit {CONCURRENCY_CONFIG}..")
-    logger.info(f"Attempting to acquire concurrency limit slot..")
-
-    async with concurrency(CONCURRENCY_CONFIG, occupy=1):
-
-        logger.info(f"Concurrency limit slot acquired..")
-
-        time.sleep(600)
-
-        logger.info(f"Concurrency slot released..")
-
-    return None
-
-
-@flow(name="Concurrency_Test_flow")
-def con_flow():
+@flow
+async def concurrent_flow(tasks):
     logger = get_run_logger()
+    logger.info("Proccessing tasks: %s", tasks)
+    async with concurrency("local_test", occupy=len(tasks)):
+        results = [long_running_task(n) for n in tasks]
+        await asyncio.gather(*results)
 
-    logger.info("Concurrency Task")
-    df = long_running_task()
+@flow(name="Concurrency_Test_flow_2", timeout_seconds=os.environ.get("TIMEOUT_SECONDS", 60))
+async def con_flow():
+    tasks_groups = np.array_split(range(40), 5)
+    print("Concurrency Task")
+    for tasks in tasks_groups:
+        await concurrent_flow(tasks)
 
-
-    return
 
 if __name__ == "__main__":
-    con_flow.from_source(
-        source=GitRepository(url="https://github.com/masonmenges/mm2-sanbox.git"),
-        entrypoint="flows/concurrency.py:con_flow",
-    ).deploy(
-        name="concurrency-test",
-        work_pool_name="docker-test",
-        image=DeploymentImage(
-                    name="masonm2/temprepo:hello_flow",
-                    dockerfile="./Dockerfile",
-                ),
-        schedule=CronSchedule(cron="0/5 * * * *", timezone="America/Denver")
-
-    )
-
-
-    params=dict(
-        name=get_deployment_name(flow.name),
-        description=flow_description,
-        work_pool_name=pool_name,
-        work_queue_name=queue_name,
-        image=image,
-        version=version,
-        build=False,
-        push=False,
-        schedule=schedule,
-        job_variables={
-            "memory_limit": 128,
-            "cpu_limit": 250
-        }
-    )
+    start = datetime.datetime.now()
+    asyncio.run(con_flow())
+    end = datetime.datetime.now()
+    print(f"runtime: {end - start}")
