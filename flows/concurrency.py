d4ceefbae045b75101b4fad2ed7eaf8576dcc6fc
masonmenges <mason.menges2@gmail.com>
2024-07-23 10:44:26 -0600

    update concurrency without timeout

--- flows/concurrency.py
+++ flows/concurrency.py
@@ -1,32 +1,44 @@
 from prefect import flow, task, get_run_logger
-import os
-from prefect.concurrency.asyncio import concurrency, rate_limit
+from prefect.concurrency.asyncio import concurrency
 import datetime
 import asyncio
 import numpy as np
 
 
 @task
-async def long_running_task(n: int):
-    logger = get_run_logger()
-    logger.info(f"Task {n} running..")
-    await asyncio.sleep(5)
+async def group_a_task(n: int):
+    # logger = get_run_logger()
+    # logger.info(f"sleep for {n}..")
+    print(f"sleep for {n}..")
+    await asyncio.sleep(n)
+    return n+1
+
+@task
+async def group_b_task(n: int):
+    # logger = get_run_logger()
+    # logger.info(f"sleep for {n}..")
+    print(f"sleep for {n}..")
+    await asyncio.sleep(n)
 
 @flow
 async def concurrent_flow(tasks):
-    logger = get_run_logger()
-    logger.info("Proccessing tasks: %s", tasks)
-    async with concurrency("local_test", occupy=len(tasks)):
-        results = [long_running_task(n) for n in tasks]
-        await asyncio.gather(*results)
+    # logger = get_run_logger()
+    # logger.info("Proccessing tasks: %s", tasks)
+    # async with concurrency("local_test", occupy=len(tasks)):
 
-@flow(name="Concurrency_Test_flow_2", timeout_seconds=os.environ.get("TIMEOUT_SECONDS", 60))
+    group_a = [group_a_task(5) for n in tasks]
+    group_a_results = await asyncio.gather(*group_a)
+
+    group_b = [group_b_task(n) for n in group_a_results]
+    await asyncio.gather(*group_b)
+
+
+@flow(name="Concurrency_Test_flow_2")
 async def con_flow():
     tasks_groups = np.array_split(range(40), 5)
     print("Concurrency Task")
-    for tasks in tasks_groups:
-        await concurrent_flow(tasks)
-
+    subflows = [concurrent_flow(tasks) for tasks in tasks_groups]
+    await asyncio.gather(*subflows)
 
 if __name__ == "__main__":
     start = datetime.datetime.now()
