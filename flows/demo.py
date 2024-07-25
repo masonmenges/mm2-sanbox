030bd6cf3eb229a48c4590d2909a55fa0c369dbc
masonmenges <mason.menges2@gmail.com>
2024-07-25 08:47:49 -0600

    update concurrency without timeout

--- flows/demo.py
+++ flows/demo.py
@@ -1,4 +1,6 @@
 from prefect import flow, task, get_run_logger
+from prefect.runner.storage import GitRepository
+from prefect.deployments import DeploymentImage
 import asyncio
 from prefect import get_client
 from prefect.client.schemas.filters import DeploymentFilterId, FlowRunFilterStateName, FlowRunFilter, DeploymentFilter
@@ -23,13 +25,30 @@ from prefect.client.schemas.sorting import FlowRunSort
         # return flow_runs[0].start_time      
 
 @flow(retries=2, log_prints=True)
-async def demo_flow(date: str):
+async def demo_flow(date: str = None):
     logger = get_run_logger()
-    logger.info(f"[green]{date}[/]")
+    logger.info(f"Hello World! {date}")
     # last_run_start_time = await compute_task()
 
 
 
 if __name__ == "__main__":
-    state = asyncio.run(demo_flow("02/02/2022"))
+    demo_flow.from_source(
+        source=GitRepository(
+            url="https://github.com/masonmenges/mm2-sanbox.git"
+            ),
+        entrypoint="flows/demo.py:demo_flow",
+    ).deploy(
+    name="params-test",
+    work_pool_name="k8s-minikube-test",
+    version="local_demo:0.0.5",
+    enforce_parameter_schema=False,
+    parameters=dict(date="2024-05-21"),
+    image="masonm2/temprepo:demo_flow",
+    build=False,
+    job_variables=dict(
+        service_account_name="prefect-test",
+        env=dict(ENV_VAR_1="value1", ENV_VAR_2="value2")
+        )
+    )
 
