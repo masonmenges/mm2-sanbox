a8ef3b099c1a870c7c6f38803f576ac5179b7c68
masonmenges <mason.menges2@gmail.com>
2024-08-02 10:21:49 -0600

    add subflow for caching test5

--- flows/test_result_persistence.py
+++ flows/test_result_persistence.py
@@ -3,18 +3,28 @@ from datetime import timedelta
 from prefect import flow, task, get_client
 from prefect.runner.storage import GitRepository
 from prefect_aws import S3Bucket
+from prefect.runtime import flow_run
+from prefect.tasks import task_input_hash
+from prefect.utilities.hashing import hash_objects
 
 
 def cache_key_from_parent(context, parameters):
     subflow_id = context.task_run.flow_run_id
-    # client = get_client()
-    # sub_flow = client.read_flow_run(subflow_id)
-    print(parameters["some_param"])
-    return parameters["some_param"]
+    # parent_id = flow_run.get_parent_flow_run_id()
+    if parameters["parent_run_id"]:
+        print("Parent run id is not None")
+        return f"{parameters}"
+    
+    return hash_objects(
+        context.task.task_key,
+        parameters
+        )
+
 
 @flow
 def subflow_caching():
-    passing_task()
+    print(flow_run.get_parent_flow_run_id())
+    passing_task(parent_run_id=flow_run.get_parent_flow_run_id())
 
 @flow(
         log_prints=True,
@@ -23,14 +33,15 @@ def subflow_caching():
         retries=1
         )
 def persist_test():
-    passing_task()
+    print(flow_run.get_parent_flow_run_id())
+    passing_task(parent_run_id=flow_run.get_parent_flow_run_id())
     subflow_caching()
 
 @task(persist_result=True,
       cache_key_fn=cache_key_from_parent,
       cache_expiration=timedelta(days=1)
       )
-def passing_task(some_param: int = 42):
+def passing_task(some_param: int = 42, parent_run_id=None):
     print("This task should be skipped on retry")
     return 42
 
@@ -39,12 +50,13 @@ def failing_task():
     raise ValueError("This task failed")
 
 if __name__=="__main__":
-    persist_test.from_source(
-            source=GitRepository(
-                url="https://github.com/masonmenges/mm2-sanbox.git"
-                ),
-            entrypoint="flows/test_result_persistence.py:persist_test",
-        ).deploy(
-        name="result_persistence-test_3.x",
-        work_pool_name="default",
-        )
+    persist_test()
+    # persist_test.from_source(
+    #         source=GitRepository(
+    #             url="https://github.com/masonmenges/mm2-sanbox.git"
+    #             ),
+    #         entrypoint="flows/test_result_persistence.py:persist_test",
+    #     ).deploy(
+    #     name="result_persistence-test_3.x",
+    #     work_pool_name="default",
+    #     )
