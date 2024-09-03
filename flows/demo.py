306e710bb69b361e27f93919bd7142c9ab1706cb
masonmenges <mason.menges2@gmail.com>
2024-09-03 11:50:18 -0600

    remove deploy code

--- flows/demo.py
+++ flows/demo.py
@@ -1,10 +1,5 @@
 from prefect import flow, task, get_run_logger
-from prefect.runner.storage import GitRepository
-from prefect.deployments import DeploymentImage
-import asyncio
-from prefect import get_client
-from prefect.client.schemas.filters import DeploymentFilterId, FlowRunFilterStateName, FlowRunFilter, DeploymentFilter
-from prefect.client.schemas.sorting import FlowRunSort
+
 
 # from state_change_hooks import cancel_if_already_running
 
@@ -33,22 +28,5 @@ async def demo_flow(date: str = None):
 
 
 if __name__ == "__main__":
-    demo_flow.from_source(
-        source=GitRepository(
-            url="https://github.com/masonmenges/mm2-sanbox.git"
-            ),
-        entrypoint="flows/demo.py:demo_flow",
-    ).deploy(
-    name="params-test",
-    work_pool_name="k8s-minikube-test",
-    version="local_demo:0.0.5",
-    enforce_parameter_schema=False,
-    parameters=dict(date="2024-05-21"),
-    image="masonm2/temprepo:demo_flow",
-    build=False,
-    job_variables=dict(
-        service_account_name="prefect-test",
-        env=dict(ENV_VAR_1="value1", ENV_VAR_2="value2")
-        )
-    )
+    demo_flow()
 
