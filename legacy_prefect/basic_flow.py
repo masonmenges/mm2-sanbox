2ef9cf6ccb291c78ef2ae762e30d5a502b8ede6e
masonmenges <mason.menges2@gmail.com>
2024-09-20 13:21:15 -0600

    add rand_failure func

--- legacy_prefect/basic_flow.py
+++ legacy_prefect/basic_flow.py
@@ -5,10 +5,8 @@ from prefect.runner.storage import GitRepository
 from random import randint
 
 
-@flow(retries=3, retry_delay_seconds=5)
-def basic_flow():
+def rand_failure(retry_count=3):
     randnum = randint(0, 1)
-    retry_count = 3
     for i in range(retry_count):
         try:
             print(f"Attempt {i + 1} of {retry_count}")
@@ -19,23 +17,30 @@ def basic_flow():
             print(f"Error: {e}")
             randnum = randint(0, 1)
             continue
+    return True
 
+
+@flow(retries=3, retry_delay_seconds=5, log_prints=True)
+def basic_flow():
+    
+    rand_failure()
+    
     return "This is a success"
 
 if __name__ == "__main__":
-    # basic_flow()
+    basic_flow()
 
-    basic_flow.from_source(
-        source=GitRepository(
-            url="https://github.com/masonmenges/mm2-sanbox.git"
-            ),
-        entrypoint="legacy_prefect/basic_flow.py:basic_flow",
-    ).deploy(
-    name="basic_flow_retries",
-    work_pool_name="k8s-minikube-test",
-    image=DockerImage(
-    name="masonm2/temprepo:basic_flow",
-    dockerfile="Dockerfile",
-    )
-    )
+    # basic_flow.from_source(
+    #     source=GitRepository(
+    #         url="https://github.com/masonmenges/mm2-sanbox.git"
+    #         ),
+    #     entrypoint="legacy_prefect/basic_flow.py:basic_flow",
+    # ).deploy(
+    # name="basic_flow_retries",
+    # work_pool_name="k8s-minikube-test",
+    # image=DockerImage(
+    # name="masonm2/temprepo:basic_flow",
+    # dockerfile="Dockerfile",
+    # )
+    # )
 
