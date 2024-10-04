6e39728f92d700ae2deac37d6806a697cc5e18dc
masonmenges <mason.menges2@gmail.com>
2024-10-04 11:40:57 -0600

    cache test 2 add map14

--- flows/task_caching_pt2.py
+++ flows/task_caching_pt2.py
@@ -35,7 +35,7 @@ def majo_3(param):
 def majo_v2(prev: str = None):
     p = [{"par": "first"},{"par": "second"}]
     f = majo_test.with_options(task_run_name="majo_test")()
-    g = majo_2.with_options(task_run_name="majo_2").submit(wait_for=[f]).wait()
+    g = majo_2.with_options(task_run_name="majo_2").submit(wait_for=[f]).result()
     h = majo_3.with_options(task_run_name="majo-[{param[par]}]").map(param=p, wait_for=[g])
     i = [mapped_run.result() for mapped_run in h]
     if f.is_failed() or g.is_failed() or any(state.is_failed() for state in i):
