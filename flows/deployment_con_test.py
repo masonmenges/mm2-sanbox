74a28659b5a8524300a4eef3a197517bf4d87c07
masonmenges <mason.menges2@gmail.com>
2024-10-07 14:53:11 -0600

    add deployment concurrency flow

---
+++ flows/deployment_con_test.py
@@ -0,0 +1,28 @@
+from prefect import flow, task
+from prefect.runner.storage import GitRepository
+import time
+
+@task
+def some_compute_task():
+    time.sleep(1800) # Simulate a long running task
+    return True
+
+@flow
+def main_flow():
+    some_compute_task()
+
+if __name__ == "__main__":
+    main_flow.from_source(
+        source=GitRepository(
+            url="https://github.com/masonmenges/mm2-sanbox.git",
+            branch="main"
+            ),
+        entrypoint="flows/deployment_con_test.py:main_flow",
+    ).deploy(
+        name="Deployment_con_test",
+        work_pool_name="k8s-minikube-test",
+        image="masonm2/temprepo:demo_3",
+        build=False,
+        push=False,
+        concurrency_limit=1
+    )
