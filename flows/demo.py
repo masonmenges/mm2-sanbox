be482afc940a9136d0c0ce7259675f825dc6dcc5
masonmenges <mason.menges2@gmail.com>
2024-10-08 14:45:28 -0600

    simplify demo flow

--- flows/demo.py
+++ flows/demo.py
@@ -1,32 +1,22 @@
 from prefect import flow, task, get_run_logger
+from prefect.runner.storage import GitRepository  
 
-
-# from state_change_hooks import cancel_if_already_running
-
-
-# @task
-# async def compute_task():
-#     async with get_client() as client:
-        # dep = await client.read_deployment("799c5f9f-7e84-4257-8f79-493b494ba244")
-
-
-
-        # flow_runs = await client.read_flow_runs(
-        #     flow_run_filter=FlowRunFilter(state=FlowRunFilterStateName(any_ = ["Completed"])),
-        #     deployment_filter=DeploymentFilter(id = DeploymentFilterId(any_ = ["799c5f9f-7e84-4257-8f79-493b494ba244"])),
-        #     sort=FlowRunSort.START_TIME_DESC,
-        #     )
-        
-        # return flow_runs[0].start_time      
-
-@flow(retries=2, log_prints=True)
+@flow(log_prints=True)
 async def demo_flow(date: str = None):
     logger = get_run_logger()
     logger.info(f"Hello World! {date}")
-    # last_run_start_time = await compute_task()
 
 
 
 if __name__ == "__main__":
-    demo_flow()
+    demo_flow.from_source(
+        source=GitRepository(
+            url="https://github.com/masonmenges/mm2-sanbox.git",
+            branch="main"
+            ),
+        entrypoint="flows/demo.py:demo_flow"
+        ).deploy(
+            name="Prefect_managed_deployment_test",
+            work_pool_name="mm2-test"
+        )
 
