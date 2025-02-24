18aad5047374042bd46de1e87536efb8966fc78c
masonmenges <mason.menges2@gmail.com>
2025-02-24 11:59:08 -0700

    gcp env vars test

--- flows/env_vars.py
+++ flows/env_vars.py
@@ -1,9 +1,18 @@
 from prefect import flow, get_run_logger
 from prefect.runner.storage import GitRepository  
+from prefect_gcp.secret_manager import GcpSecret
 import os
+
+# from flows.task_caching import some_compute_task
 
 @flow(log_prints=True)
 def env_vars_flow():
+
+    logger = get_run_logger()
+
+    gcpsecret_block = GcpSecret.load("mm2-test-secret")
+    logger.info(gcpsecret_block.read_secret())
+
     print(os.environ.get("yaml_env_var_1"))
     print(os.environ.get("yaml_env_var_2"))
     print(os.environ.get("env_var_job_var_1"))
@@ -18,8 +27,8 @@ if __name__ == "__main__":
         ),
         entrypoint="flows/env_vars.py:env_vars_flow"
     ).deploy(
-        name="prefect_env_vars_test",
-        work_pool_name="ecs-test-pool",
+        name="prefect_env_vars_test_vertex_ai",
+        work_pool_name="mm2-vertextai-testing",
         job_variables={ "env": {
             "env_var_job_var_1": "job_value_1",
             "env_var_job_var_2": "job_value_2"
