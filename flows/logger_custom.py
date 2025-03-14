47b8e067a09441fbca6cff6880c9929cdbd94893
masonmenges <mason.menges2@gmail.com>
2025-03-14 12:14:18 -0600

    custom loggers test2

--- flows/logger_custom.py
+++ flows/logger_custom.py
@@ -1,14 +1,10 @@
-from prefect import flow, get_run_logger
 import logging
 
 logger = logging.getLogger("foo")
 logger.setLevel(logging.INFO)
 
  
-@flow
 def bar():
-  prefect_logger = get_run_logger()
   logger.info("this is foo")
-  prefect_logger.info("this is the prefect logger")
 
 
--- flows/logger_custom_flow.py
+++ flows/logger_custom_flow.py
@@ -1,4 +1,14 @@
 import logger_custom as foo
 
-foo.bar()
+from prefect import flow, get_run_logger
+
+@flow
+def logging_test_flow():
+    prefect_logger = get_run_logger()
+    foo.bar()
+    prefect_logger.info("This is the prefect logger")
  
+
+
+if __name__ == "__main__":
+    logging_test_flow()
