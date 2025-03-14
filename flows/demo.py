184fcc8677ba57be12d57a6268c9ea15c85d732b
masonmenges <mason.menges2@gmail.com>
2025-03-14 10:12:20 -0600

    extra params

--- flows/demo.py
+++ flows/demo.py
@@ -3,22 +3,23 @@ from prefect.runner.storage import GitRepository
 import asyncio
 
 @flow(log_prints=True)
-async def demo_flow(date: str = None):
+async def demo_flow(date: str = None, configs: dict = None):
     logger = get_run_logger()
     logger.info(f"Hello World! {date}")
+    logger.info(f"Config name: {configs["name"]}")
 
 
 
 if __name__ == "__main__":
-    # demo_flow.from_source(
-    #     source=GitRepository(
-    #         url="https://github.com/masonmenges/mm2-sanbox.git",
-    #         branch="main"
-    #         ),
-    #     entrypoint="flows/demo.py:demo_flow"
-    #     ).deploy(
-    #         name="Prefect_managed_deployment_test",
-    #         work_pool_name="mm2-test"
-    #     )
+    demo_flow.from_source(
+        source=GitRepository(
+            url="https://github.com/masonmenges/mm2-sanbox.git",
+            branch="main"
+            ),
+        entrypoint="flows/demo.py:demo_flow"
+        ).deploy(
+            name="Prefect_server_deployment_test_2",
+            work_pool_name="local-server-test"
+        )
 
-    asyncio.run(demo_flow("2024-11-14"))
+    # asyncio.run(demo_flow("2024-11-14"))
