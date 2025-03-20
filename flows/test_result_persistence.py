275d237f89a9f2224d90b0bb9769ed980e006792
masonmenges <mason.menges2@gmail.com>
2025-03-20 09:04:39 -0600

    update result persistence test3

--- flows/test_result_persistence.py
+++ flows/test_result_persistence.py
@@ -1,39 +1,31 @@
-from datetime import timedelta
-
-from prefect import flow, task, get_client
+from prefect import flow, task
 from prefect.runner.storage import GitRepository
-from prefect_aws import S3Bucket
-from prefect.runtime import flow_run
-from prefect.tasks import task_input_hash
-from prefect.utilities.hashing import hash_objects
+from prefect_aws import S3Bucket, AwsCredentials
 
+aws_creds = AwsCredentials.load("mm2-se-dev", _sync=True)
 
-@flow
-def subflow_caching():
-    print(flow_run.get_parent_flow_run_id())
-    passing_task(parent_run_id=flow_run.get_parent_flow_run_id())
+result_storage = S3Bucket(
+    bucket_name="mm2-results",
+    bucket_folder="Prefect3",
+    credentials=aws_creds
+)
+result_storage.save(name="mm2-results3", overwrite=True, _sync=True)
 
 @flow(
         log_prints=True,
-        result_storage=S3Bucket.load("mm2-prefect-s3"),
+        result_storage=result_storage,
         persist_result=True,
         retries=1
         )
 def persist_test():
-    passing_task(parent_run_id=flow_run.get_parent_flow_run_id())
-    subflow_caching()
-
-    return 42
+    success_task = passing_task()
+    return success_task
 
 @task(persist_result=True
       )
 def passing_task(some_param: int = 42, parent_run_id=None):
     print("This task should be skipped on retry")
     return 42
-
-@task(persist_result=True)
-def failing_task():
-    raise ValueError("This task failed")
 
 if __name__=="__main__":
     # persist_test()
@@ -45,4 +37,7 @@ if __name__=="__main__":
         ).deploy(
         name="result_persistence-test_3.x",
         work_pool_name="k8s-minikube-test",
+        image="masonm2/temprepo:withcode03132025.2",
+        build=False,
+        push=False
         )
