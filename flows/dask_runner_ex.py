986bdbd32db139993ac1e1c56b3874efb7fddccb
masonmenges <mason.menges2@gmail.com>
2025-04-25 12:05:48 -0600

    add dask runner flow

---
+++ flows/dask_runner_ex.py
@@ -0,0 +1,24 @@
+from prefect import flow, task
+from random import randint
+import time
+import asyncio
+
+@task
+async def my_task(time_seconds: int):
+     time.sleep(time_seconds)
+     return time_seconds
+
+@flow(log_prints=True
+     )
+async def my_flow():
+     ran_nums = [10, 11, 12, 13]
+     futures = []
+
+     futures = my_task.map(ran_nums)
+
+     results = futures.result()
+     print(results)
+
+
+if __name__ == "__main__":
+    asyncio.run(my_flow())
