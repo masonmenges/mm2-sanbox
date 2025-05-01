23783f478c278271ad001a19c15d51a0494b10c6
masonmenges <mason.menges2@gmail.com>
2025-05-01 12:54:50 -0600

    update concurrency flow

--- flows/concurrency.py
+++ flows/concurrency.py
@@ -1,44 +1,68 @@
 from prefect import flow, task, get_run_logger
 from prefect.concurrency.asyncio import concurrency
+from prefect.context import get_run_context
 import datetime
 import asyncio
 import numpy as np
 
+@task
+async def group_a_task(n: int, run_name: str):
+
+    count = 0
+    a = 0
+    b = 1
+
+    sequence = []
+    while count < n:
+        sequence.append(a)
+        ab = a + b
+        a = b
+        b = ab
+        count +=1
+
+    print(f"{run_name}:group a fibinacci sequence for {n} is {sequence}")
+
+
+    return n + 1
 
 @task
-async def group_a_task(n: int):
+async def group_b_task(n: int, run_name: str):
     # logger = get_run_logger()
     # logger.info(f"sleep for {n}..")
-    print(f"sleep for {n}..")
-    await asyncio.sleep(n)
-    return n+1
+    count = 0
+    a = 0
+    b = 1
 
-@task
-async def group_b_task(n: int):
-    # logger = get_run_logger()
-    # logger.info(f"sleep for {n}..")
-    print(f"sleep for {n}..")
-    await asyncio.sleep(n)
+    sequence = []
+    while count < n:
+        sequence.append(a)
+        ab = a + b
+        a = b
+        b = ab
+        count +=1
 
-@flow
+    print(f"{run_name}:group b fibinacci sequence for {n} is {sequence}")
+
+@flow()
 async def concurrent_flow(tasks):
-    # logger = get_run_logger()
-    # logger.info("Proccessing tasks: %s", tasks)
-    # async with concurrency("local_test", occupy=len(tasks)):
 
-    group_a = [group_a_task(5) for n in tasks]
+    flow_run_name = get_run_context().flow_run.name
+
+    group_a = [group_a_task(n, flow_run_name) for n in tasks]
     group_a_results = await asyncio.gather(*group_a)
 
-    group_b = [group_b_task(n) for n in group_a_results]
+    group_b = [group_b_task(n, flow_run_name) for n in group_a_results]
     await asyncio.gather(*group_b)
 
 
+
 @flow(name="Concurrency_Test_flow_2")
-async def con_flow():
-    tasks_groups = np.array_split(range(40), 5)
-    print("Concurrency Task")
+async def con_flow(max_n: int = 10):
+    tasks_groups = np.array_split(range(max_n), 2)
     subflows = [concurrent_flow(tasks) for tasks in tasks_groups]
-    await asyncio.gather(*subflows, return_exceptions=True)
+    test = await asyncio.gather(*subflows, return_exceptions=True)
+
+
 
 if __name__ == "__main__":
     start = datetime.datetime.now()
