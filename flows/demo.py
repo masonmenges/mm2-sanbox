eb9f95788f73d7cbd5659fdd5359b67efa833078
masonmenges <mason.menges2@gmail.com>
2025-05-20 10:13:56 -0600

    update flow params

--- flows/demo.py
+++ flows/demo.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from prefect import flow, task, get_run_logger
 from prefect.runner.storage import GitRepository  
 from prefect.artifacts import create_link_artifact
-import asyncio
+import asyncio, os
 
 class SampleDropdownEnum(str, enum.Enum):
     positive="positvie"
@@ -18,7 +18,7 @@ class SampleValues(BaseModel):
 
 
 @flow(log_prints=True)
-async def demo_flow(date: str = "2025-05-20"):
+async def demo_flow(date: str = "2025-05-15"):
     logger = get_run_logger()
     logger.info(f"Configs date: {date}")
     await create_link_artifact(link="notreal.notrel")
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     demo_flow.from_source(
         source=GitRepository(
             url="https://github.com/masonmenges/mm2-sanbox.git",
-            branch="main"
+            commit_sha=os.getenv("GITHUB_SHA")
             ),
         entrypoint="flows/demo.py:demo_flow"
         ).deploy(
@@ -40,4 +40,3 @@ if __name__ == "__main__":
         )
     open_api_schema = demo_flow.to_deployment(name="false")._parameter_openapi_schema.model_dump()
     print(open_api_schema)
-    asyncio.run(demo_flow("2024-11-14"))
