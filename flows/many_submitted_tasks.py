5ce53772596e368808a61d44c6555641584b87db
masonmenges <mason.menges2@gmail.com>
2025-07-02 12:48:13 -0600

    submitted tasks change failure mode

--- flows/many_submitted_tasks.py
+++ flows/many_submitted_tasks.py
@@ -3,14 +3,14 @@ from prefect import flow, task
 
 @task
 def f(x):
-    if x > 10:
+    if x%2 > 1:
         raise
 
 
 @flow
 def test_flow():
     futures = []
-    for i in range(0, 15):
+    for i in range(0, 60):
         futures.append(f.submit(i))
 
     return futures
