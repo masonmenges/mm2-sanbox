aa6a022235618395631585d2e2271a4943f25441
masonmenges <mason.menges2@gmail.com>
2025-07-10 12:44:34 -0600

    add flow utils

--- flows/dynamic_task_runner.py
+++ flows/dynamic_task_runner.py
@@ -7,14 +7,12 @@ from flow_utils.deployment_steps import get_num_workers_value
 
 num_workers = get_num_workers_value()
 
-    
 @task
 def some_task():
     pass
 
 @flow(task_runner= DaskTaskRunner(cluster_kwargs={"n_workers": num_workers}))
 def dynamic_task_runner_flow():
-
     some_task.submit().wait()
 
 if __name__ == "__main__":
