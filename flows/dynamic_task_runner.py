1bb34bd543468f8b60f306c2081ef9d6c92523c6
masonmenges <mason.menges2@gmail.com>
2025-07-10 12:43:06 -0600

    remove on_running_hook

--- flows/dynamic_task_runner.py
+++ flows/dynamic_task_runner.py
@@ -3,14 +3,16 @@ import os
 from prefect import flow, task
 from prefect_dask import DaskTaskRunner
 from prefect.variables import Variable
-from prefect import context
+from flow_utils.deployment_steps import get_num_workers_value
+
+num_workers = get_num_workers_value()
 
     
 @task
 def some_task():
     pass
 
-@flow(task_runner= DaskTaskRunner(cluster_kwargs={"n_workers": int(os.environ.get("num_workers", "1"))}))
+@flow(task_runner= DaskTaskRunner(cluster_kwargs={"n_workers": num_workers}))
 def dynamic_task_runner_flow():
 
     some_task.submit().wait()
