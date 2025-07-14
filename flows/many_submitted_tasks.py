98ef8afcd4910f92a20ec2ad41df6e10f8bbb65a
masonmenges <mason.menges2@gmail.com>
2025-07-14 07:38:47 -0600

    limit task number to 10

--- flows/many_submitted_tasks.py
+++ flows/many_submitted_tasks.py
@@ -4,14 +4,14 @@ from prefect_shell import shell_run_command
 
 @task
 def f(x):
-    shell_run_command.submit(
+    shell_run_command.with_options(timeout_seconds=600).submit(
         command="sling run -r misc/sling_test.yaml"
     ).wait()
 
 @flow
 def test_flow():
     futures = []
-    for i in range(0, 60):
+    for i in range(0, 10):
         futures.append(f.submit(i))
 
     return futures
