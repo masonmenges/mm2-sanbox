37feaedb8d765fd35b18760fe8274c681be800e2
masonmenges <mason.menges2@gmail.com>
2025-07-14 12:22:12 -0600

    removed unused arg

--- flows/many_submitted_tasks.py
+++ flows/many_submitted_tasks.py
@@ -38,15 +38,11 @@ def create_duckdb(n):
 @task
 def f(csv_file_name, duck_db):
 
-    cmd = f"sling run --src-stream 'file://{csv_file_name}' --src-options 'format: csv' --tgt-conn {duck_db} --tgt-object 'test' --mode full-refresh"
+    cmd = f"sling run --src-stream 'file://{csv_file_name}' --tgt-conn {duck_db} --tgt-object 'test' --mode full-refresh"
 
     shell_run_command.with_options(timeout_seconds=600).submit(
         command=cmd
     ).wait()
-
-    # print("!!!!!!!!!!TESTING!!!!!!!!")
-    # print(test)
-    # print("!!!!!!!!!!TESTING!!!!!!!!")
 
 @flow
 def test_flow():
