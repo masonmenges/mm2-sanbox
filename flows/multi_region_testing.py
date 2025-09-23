721e06393eb391ba672d54619decef67db350b10
masonmenges <mason.menges2@gmail.com>
2025-09-23 12:22:46 -0600

    increase sleep time

--- flows/multi_region_testing.py
+++ flows/multi_region_testing.py
@@ -3,10 +3,11 @@ from prefect.context import get_run_context
 from prefect.runner.storage import GitRepository  
 from prefect.concurrency.sync import concurrency
 import os, time
+from pprint import pprint
 
 @task(retries=2, log_prints=True)
 def some_task():
-    time.sleep(2)
+    time.sleep(20)
     context = get_run_context()
     print(context.task_run.run_count)
     if context.task_run.run_count > 1:
@@ -24,22 +25,22 @@ def demo_flow():
 
 
 if __name__ == "__main__":
-    demo_flow.from_source(
-        source=GitRepository(
-            url="https://github.com/masonmenges/mm2-sanbox.git",
-            commit_sha=os.getenv("GITHUB_SHA")
-            ),
-        entrypoint="flows/demo.py:demo_flow"
-        ).deploy(
-            name="dynamic-parameter-test",
-            work_pool_name="demo_eks",
-            job_variables={
-                "env": {
-                    "PREFECT_FLOW_RUN_EXECUTE_SIGTERM_BEHAVIOR": "false"
-                },
-                "backoff_limit": 1
-            }
-        )
+    # demo_flow.from_source(
+    #     source=GitRepository(
+    #         url="https://github.com/masonmenges/mm2-sanbox.git",
+    #         commit_sha=os.getenv("GITHUB_SHA")
+    #         ),
+    #     entrypoint="flows/multi_region_testing.py:demo_flow"
+    #     ).deploy(
+    #         name="dynamic-parameter-test",
+    #         work_pool_name="demo_eks",
+    #         job_variables={
+    #             "env": {
+    #                 "PREFECT_FLOW_RUN_EXECUTE_SIGTERM_BEHAVIOR": "false"
+    #             },
+    #             "backoff_limit": 1
+    #         }
+    #     )
     # open_api_schema = demo_flow.to_deployment(name="false")._parameter_openapi_schema.model_dump()
     # pprint(open_api_schema)
-    # asyncio.run(demo_flow())
+    demo_flow()
