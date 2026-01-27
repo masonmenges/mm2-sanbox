285a8956ac055fd3558993f369fc5d8846579473
masonmenges <mason.menges2@gmail.com>
2026-01-27 12:46:22 -0700

    update task_caching ex

--- flows/task_caching.py
+++ flows/task_caching.py
@@ -1,18 +1,25 @@
+import os
+
 from prefect import flow, task, get_run_logger
-# from prefect.cache_policies import DEFAULT
 from prefect_aws import S3Bucket
 
 from prefect.runner.storage import GitRepository
 from prefect.client.schemas.schedules import CronSchedule
 import time
 
+from prefect.cache_policies import DEFAULT, NONE, INPUTS
+
+
 s3_bucket = S3Bucket.load("mm2-prefect-s3", _sync=True)
 s3_bucket.bucket_folder = "cache_key"
-# cache_config = DEFAULT.configure(
-#     key_storage=s3_bucket,
-# )
+cache_config = INPUTS.configure(
+    key_storage=s3_bucket,
+)
 
-@task
+refresh_cache = os.getenv("REFRESH_CACHE", "False")
+
+
+@task(cache_policy=cache_config, refresh_cache=refresh_cache)
 def some_compute_task(a_number: int):
     logger = get_run_logger()
 
@@ -20,13 +27,15 @@ def some_compute_task(a_number: int):
     time.sleep(1800)
     return a_number + 1
 
-@flow(persist_result=True, result_storage=S3Bucket.load("mm2-prefect-s3"))
-def main_flow(a_number: int = 1):
+@task(cache_policy=cache_config, refresh_cache=refresh_cache)
+def sub_flow(a_number: int):
     some_compute_task(a_number)
 
+@flow(persist_result=True)
+def main_flow(a_number: int = 1):
+    sub_flow(a_number)
+
 if __name__ == "__main__":
-    from prefect.runner.storage import GitRepository
-    from prefect.client.schemas.schedules import CronSchedule
     main_flow.from_source(
         source=GitRepository(
             url="https://github.com/masonmenges/mm2-sanbox.git",
@@ -34,10 +43,7 @@ if __name__ == "__main__":
             ),
         entrypoint="flows/task_caching.py:main_flow",
     ).deploy(
-        name="task_caching",
+        name="task_caching_ex",
         work_pool_name="k8s-minikube-test",
-        image="masonm2/temprepo:demo_3",
-        build=False,
-        push=False,
-        schedules=[CronSchedule(cron="0 0 * * *")],
+        image="masonm2/temprepo:demo_3"
     )
